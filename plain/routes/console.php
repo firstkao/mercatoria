6a80987a30c21833e22aa9dbdb8be17752cca40f
firstkao <118149621+firstkao@users.noreply.github.com>
2026-09-26 01:01:02 +0700

    Update console.php

--- plain/routes/console.php
+++ plain/routes/console.php
@@ -2,5 +2,6 @@
 
 use Illuminate\Support\Facades\Schedule;
 
+Schedule::command('orders:cancel-unpaid')->hourly();
 Schedule::command('spammers:prune-expired')->hourly()->withoutOverlapping(30);
 Schedule::command('activity-logs:prune')->dailyAt('03:00')->timezone('Asia/Jakarta');
